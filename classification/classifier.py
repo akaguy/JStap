0890e539e5721ac8bb9de7071abd21af9862d1a0
Guy Matzliach <gmatzlia@akamai.com>
2020-09-01 15:27:17 +0300

    Move loading args to main function

--- classification/classifier.py
+++ classification/classifier.py
@@ -116,6 +116,7 @@ def parsing_commands():
 
     return vars(parser.parse_args())
 
+arg_obj = {}
 
 def main_classification(js_dirs=arg_obj['d'], js_files=arg_obj['f'], labels_f=arg_obj['lf'],
                         labels_d=arg_obj['l'], model=arg_obj['m'], threshold=arg_obj['th'],
